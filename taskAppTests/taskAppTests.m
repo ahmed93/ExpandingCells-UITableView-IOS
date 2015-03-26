d0e807917306d04a67abf182d80544023c022cc4
Ahmed Mohamed Magdi <hero.20112012@gmail.com>
2015-03-26 11:50:21 +0200

    Initial Commit

---
+++ taskAppTests/taskAppTests.m
@@ -0,0 +1,40 @@
+//
+//  taskAppTests.m
+//  taskAppTests
+//
+//  Created by Ahmed Mohamed Fareed on 3/26/15.
+//  Copyright (c) 2015 Ahmed Mohamed Fareed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface taskAppTests : XCTestCase
+
+@end
+
+@implementation taskAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
